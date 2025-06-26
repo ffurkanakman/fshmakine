3651aa4ee7aa7a89a28096b6e6f15fc638e250fd
tai lung <aydindiablo@gmail.com>
2025-06-26 15:31:13 +0300

    Araç Ekle Sayfası bitti

--- resources/js/Libs/Metronic/_metronic/layout/components/aside/AsideDefault.jsx
+++ resources/js/Libs/Metronic/_metronic/layout/components/aside/AsideDefault.jsx
@@ -1,83 +1,93 @@
-import {useState} from 'react'
-import {Link} from 'react-router-dom'
-import clsx from 'clsx'
-import {useLayout} from '../../core'
-import {KTIcon, toAbsoluteUrl} from '../../../helpers'
-import {AsideTabs} from './AsideTabs'
-import {AsideFooter} from './AsideFooter'
-import {TabsBase} from './Tabs/_TabsBase'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import clsx from "clsx";
+import { useLayout } from "../../core";
+import { KTIcon, toAbsoluteUrl } from "../../../helpers";
+import { AsideTabs } from "./AsideTabs";
+import { AsideFooter } from "./AsideFooter";
+import { TabsBase } from "./Tabs/_TabsBase";
 
 const AsideDefault = () => {
-    const {config} = useLayout()
-    const {classes} = useLayout()
-    const [link, setLink] = useState('menu')
+    const { config } = useLayout();
+    const { classes } = useLayout();
+    const [link, setLink] = useState("menu");
 
     return (
         <div
-            id='kt_aside'
-            className={clsx('aside aside-extended', classes.aside.join(' '))}
-            data-kt-drawer='true'
-            data-kt-drawer-name='aside'
-            data-kt-drawer-activate='{default: true, lg: false}'
-            data-kt-drawer-overlay='false'
-            data-kt-drawer-width='auto'
-            data-kt-drawer-direction='start'
-            data-kt-drawer-toggle='#kt_aside_toggle'
+            id="kt_aside"
+            className={clsx("aside aside-extended", classes.aside.join(" "))}
+            data-kt-drawer="true"
+            data-kt-drawer-name="aside"
+            data-kt-drawer-activate="{default: true, lg: false}"
+            data-kt-drawer-overlay="false"
+            data-kt-drawer-width="auto"
+            data-kt-drawer-direction="start"
+            data-kt-drawer-toggle="#kt_aside_toggle"
         >
             {/* begin::Primary */}
-            <div className='aside-primary d-flex flex-column align-items-lg-center flex-row-auto'>
+            <div className="aside-primary d-flex flex-column align-items-lg-center flex-row-auto">
                 {/* begin::Logo */}
                 <div
-                    className='aside-logo d-none d-lg-flex flex-column align-items-center flex-column-auto py-10'
-                    id='kt_aside_logo'
+                    className="aside-logo d-none d-lg-flex flex-column align-items-center flex-column-auto py-10"
+                    id="kt_aside_logo"
                 >
-                    <Link to='/dashboard'>
-                        <img src={toAbsoluteUrl('img/vector/logo.png')} alt='logo' className='h-35px'/>
+                    <Link to="/dashboard">
+                        <img
+                            src={toAbsoluteUrl("img/vector/logo.png")}
+                            alt="logo"
+                            className="h-35px"
+                        />
                     </Link>
                 </div>
                 {/* end::Logo */}
                 {/* begin::Nav */}
                 <div
-                    className='aside-nav d-flex flex-column align-items-center flex-column-fluid w-100 pt-5 pt-lg-0'
-                    id='kt_aside_nav'
+                    className="aside-nav d-flex flex-column align-items-center flex-column-fluid w-100 pt-5 pt-lg-0"
+                    id="kt_aside_nav"
                 >
-                    <AsideTabs link={link} setLink={setLink}/>
+                    <AsideTabs link={link} setLink={setLink} />
                 </div>
                 {/* end::Nav */}
-                <AsideFooter/>
+                <AsideFooter />
             </div>
             {/* end::Primary */}
             {config.aside.secondaryDisplay && (
                 <>
                     {/* begin::Secondary */}
-                    <div className='aside-secondary d-flex flex-row-fluid'>
+                    <div className="aside-secondary d-flex flex-row-fluid">
                         {/* begin::Workspace */}
-                        <div className='aside-workspace my-5 p-5' id='kt_aside_wordspace'>
-                            <TabsBase link={link}/>
+                        <div
+                            className="aside-workspace my-5 p-5"
+                            id="kt_aside_wordspace"
+                        >
+                            <TabsBase link={link} />
                         </div>
                         {/* end::Workspace */}
                     </div>
                     {/* end::Secondary */}
                     {/* begin::Aside Toggle */}
                     <button
-                        id='kt_aside_toggle'
+                        id="kt_aside_toggle"
                         className={clsx(
-                            'btn btn-sm btn-icon bg-body btn-color-gray-700 btn-active-primary position-absolute translate-middle start-100 end-0 bottom-0 shadow-sm d-none d-lg-flex',
-                            classes.asideToggle.join(' ')
+                            "btn btn-sm btn-icon bg-body btn-color-gray-700 btn-active-primary position-absolute translate-middle start-100 end-0 bottom-0 shadow-sm d-none d-lg-flex",
+                            classes.asideToggle.join(" ")
                         )}
-                        data-kt-toggle='true'
-                        data-kt-toggle-state='active'
-                        data-kt-toggle-target='body'
-                        data-kt-toggle-name='aside-minimize'
-                        style={{marginBottom: '1.35rem'}}
+                        data-kt-toggle="true"
+                        data-kt-toggle-state="active"
+                        data-kt-toggle-target="body"
+                        data-kt-toggle-name="aside-minimize"
+                        style={{ marginBottom: "1.35rem" }}
                     >
-                        <KTIcon iconName='arrow-left' className='fs-2 rotate-180'/>
+                        <KTIcon
+                            iconName="arrow-left"
+                            className="fs-2 rotate-180"
+                        />
                     </button>
                     {/* end::Aside Toggle */}
                 </>
             )}
         </div>
-    )
-}
+    );
+};
 
-export {AsideDefault}
+export { AsideDefault };
--- resources/js/Libs/Metronic/_metronic/layout/components/aside/AsideMenuMain.jsx
+++ resources/js/Libs/Metronic/_metronic/layout/components/aside/AsideMenuMain.jsx
@@ -1,70 +1,86 @@
-import {IntlProvider} from 'react-intl'
-import {AsideMenuItemWithSub} from './AsideMenuItemWithSub'
-import {AsideMenuItem} from './AsideMenuItem'
+import { IntlProvider } from "react-intl";
+import { AsideMenuItemWithSub } from "./AsideMenuItemWithSub";
+import { AsideMenuItem } from "./AsideMenuItem";
 
 const AsideMenuMain = () => {
     const messages = {
-        'MENU.DASHBOARD': 'Gösterge Paneli'
-    }
+        "MENU.DASHBOARD": "Gösterge Paneli",
+    };
 
     return (
-        <IntlProvider messages={messages} locale='tr' defaultLocale='tr'>
+        <IntlProvider messages={messages} locale="tr" defaultLocale="tr">
             <>
                 <AsideMenuItem
-                    to='/dashboard'
-                    icon='color-swatch'
-                    title='Gösterge Paneli'
-                    fontIcon='bi-app-indicator'
+                    to="/dashboard"
+                    icon="color-swatch"
+                    title="Gösterge Paneli"
+                    fontIcon="bi-app-indicator"
                 />
                 <AsideMenuItem
-                    to='/Projeler'
-                    icon='element-11'
-                    title='Projeler'
-                    fontIcon='bi-clipboard-data'
+                    to="/Projeler"
+                    icon="element-11"
+                    title="Projeler"
+                    fontIcon="bi-clipboard-data"
                 />
                 <AsideMenuItem
-                    to='/Kullanicilar'
-                    icon='people'
-                    title='Kullanıcılar'
-                    fontIcon='bi-people'
+                    to="/Kullanicilar"
+                    icon="people"
+                    title="Kullanıcılar"
+                    fontIcon="bi-people"
                 />
                 <AsideMenuItem
-                    to='/Musteriler'
-                    icon='people'
-                    title='Müşteriler'
-                    fontIcon='bi-people'
+                    to="/Musteriler"
+                    icon="people"
+                    title="Müşteriler"
+                    fontIcon="bi-people"
                 />
                 <AsideMenuItem
-                    to='/builder'
-                    icon='switch'
-                    title='Layout Builder'
-                    fontIcon='bi-layers'
+                    to="/builder"
+                    icon="switch"
+                    title="Layout Builder"
+                    fontIcon="bi-layers"
                 />
                 <AsideMenuItem
-                    to='/Teklif'
-                    icon='bi bi-filetype-pdf'
-                    title='Teklif'
-                    fontIcon='bi-layers'
+                    to="/Teklif"
+                    icon="bi bi-filetype-pdf"
+                    title="Teklif"
+                    fontIcon="bi-layers"
+                />
+                <AsideMenuItem
+                    to="/AraclarListesi"
+                    icon="bi bi-filetype-pdf"
+                    title="Araç Ekle"
+                    fontIcon="bi-layers"
                 />
 
-                <div className='menu-item'>
-                    <div className='menu-content pt-8 pb-2'>
-                        <span className='menu-section text-muted text-uppercase fs-8 ls-1'>Ayarlar</span>
+                <div className="menu-item">
+                    <div className="menu-content pt-8 pb-2">
+                        <span className="menu-section text-muted text-uppercase fs-8 ls-1">
+                            Ayarlar
+                        </span>
                     </div>
                 </div>
 
                 <AsideMenuItemWithSub
-                    to='/settings'
-                    title='Ayarlar'
-                    icon='setting-2'
-                    fontIcon='bi-gear'
+                    to="/settings"
+                    title="Ayarlar"
+                    icon="setting-2"
+                    fontIcon="bi-gear"
                 >
-                    <AsideMenuItem to='/settings/notifications' title='Bildirim Ayarları' hasBullet={true} />
-                    <AsideMenuItem to='/settings/account' title='Hesap Ayarları' hasBullet={true} />
+                    <AsideMenuItem
+                        to="/settings/notifications"
+                        title="Bildirim Ayarları"
+                        hasBullet={true}
+                    />
+                    <AsideMenuItem
+                        to="/settings/account"
+                        title="Hesap Ayarları"
+                        hasBullet={true}
+                    />
                 </AsideMenuItemWithSub>
             </>
         </IntlProvider>
-    )
-}
+    );
+};
 
-export {AsideMenuMain}
+export { AsideMenuMain };
